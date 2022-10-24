8fbca55d44030eb8f6ad1497fef6abb2191e61e7
JoshuaTet <joshua.tetzner@gmx.de>
2022-10-24 12:55:11 +0200

    New Adder

--- Adder/adder.cpp
+++ Adder/adder.cpp
@@ -1,5 +1,4 @@
-
-using namespace std;
+#include "adder.hpp"
 
 float add(float a, float b){
     return (a + b);
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Adder/adder.hpp"
+
+int main(){
+    std::cout << "Hallo World \n";
+    std::cout << add(22.5f, 23.3f) << '\n';
+    return 0;
+}
