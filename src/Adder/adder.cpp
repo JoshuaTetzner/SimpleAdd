e0f471e1adabbff964d0ea17a60da5ebfc77a784
JoshuaTet <joshua.tetzner@gmx.de>
2022-10-25 15:19:34 +0200

    Updated structure

--- src/Adder/adder.cpp
+++ src/Adder/adder.cpp
@@ -4,6 +4,14 @@ extern "C" {
 float add(float, float);
 }
 
+extern "C" {
+int add(int, int);
+}
+
 float add(float a, float b){
+    return (a + b);
+}
+
+int add(int a, int b){
     return (a + b);
 }
